628019431b25a1e71011ef920748775f6af947d8
ArthurBitt <add2coffee@gmail.com>
2023-05-05 11:38:31 -0300

    .

---
+++ Projetos práticos/Pokedex/src/br/com/pokedex/teste.java
@@ -0,0 +1,13 @@
+package br.com.pokedex;
+
+import java.util.Scanner;
+
+public class teste {
+    public static void main(String[] args) {
+        String facu = "FIAP – A melhor faculdade de tecnologia ";
+
+        int posição = facu.indexOf("a");
+        System.out.println("O índice do caractere 'a' na string é "+ posição);
+    }
+}
+
